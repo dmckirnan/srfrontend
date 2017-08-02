9a387b67bec543b1961687a5a8f1ed6557a02ba0
Darrick <dmckirnan@hotmail.com>
2017-08-01 19:17:51 -0700

    added new stuff

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, + 'public/index.html'));
+})
+
+app.listen(PORT);
+console.log('Listening on Port 4000');
