ad606700952cbe7871cf165c1f2a75b676966db6
Darrick <dmckirnan@hotmail.com>
2017-08-02 19:02:08 -0700

    added fade-ins and fade-outs, selected class on arrow movement and clicks

--- public/main.js
+++ public/main.js
@@ -14,22 +14,35 @@ window.onload = () => {
     '3': "./images/zootopia.png",
   }
 
-  
+
   let thumbnails = [];
   for (let i = 0; i < 4; i += 1) {
     thumbnails[i] = document.createElement('div');
     thumbnails[i].setAttribute('class', 'thumbnail');
     thumbnails[i].setAttribute('style', 'background-image: url(' + imgCache[i] + '); background-repeat: no-repeat center; background-size: 150px 150px');
+    thumbnails[i].setAttribute('id', i);
+    if (i === 1) thumbnails[i].classList.add('selected');
 
+    // OnClick to Change Movies
     thumbnails[i].onclick = () => {
       let videoPlayer = document.getElementById('video');
       let currentVid = document.getElementById('videoSource');
       // Prevents clicking the same thumbnail to restart the preview
       if ('.' + currentVid.src.substring(21) !== videoCache[i]) {
+        $('#video').fadeOut();
+        $('#video').fadeOut('slow');
+        $('#video').fadeOut(2000);
         videoPlayer.pause();
-        currentVid.src = videoCache[i];
-        videoPlayer.load();
-        videoPlayer.play();
+        $('.selected').removeClass('selected');
+
+        setTimeout(() => {
+          // Resolve New Source URL and Handle Possible Edge Case of Being at End of Map
+          currentVid.src = videoCache[i];
+          videoPlayer.load();
+          videoPlayer.play();
+          $('#video').fadeIn();
+          thumbnails[i].classList.add('selected');
+        }, 500);
       }
     }
     let imageContainer = document.getElementById('imageContainer');
@@ -43,43 +56,62 @@ window.onload = () => {
 
     if (e.code === 'ArrowLeft') {
       let target;
+
+      // Find Target URL Index
       for (let x in videoCache) {
         if (videoCache[x] === current) target = x;
       }
+
+      // Find Current Image Element By Id -> Remove / Add as Necessary to Reflect Current Preview Being Watched
       let videoPlayer = document.getElementById('video');
+      let newTarget = target - 1 >= 0 ? target - 1 : 3;
+      let selected = document.getElementById(newTarget);
+      $('.selected').removeClass('selected');
+      selected.classList.add('selected');
+
+      // Fade Out Current Selection
+      $('#video').fadeOut();
+      $('#video').fadeOut('slow');
+      $('#video').fadeOut(2000);
       videoPlayer.pause();
-      console.log(videoCache[target - 1]);
-      currentVid.src = videoCache[target - 1] ? videoCache[target - 1] : videoCache[3];
-      videoPlayer.load();
-      videoPlayer.play();
+
+      // Set Time Out for Fade-In of Next Video
+      setTimeout(() => {
+        // Resolve New Source URL and Handle Possible Edge Case of Being at Beginning of Map
+        currentVid.src = videoCache[target - 1] ? videoCache[target - 1] : videoCache[3];
+        videoPlayer.load();
+        videoPlayer.play();
+        $('#video').fadeIn();
+      }, 500);
     } else if (e.code === 'ArrowRight') {
       let target;
+
+      // Find Target URL Index
       for (let x in videoCache) {
         if (videoCache[x] === current) target = x;
       }
+
+      // Find Current Image Element By Id -> Remove / Add as Necessary to Reflect Current Preview Being Watched
       let videoPlayer = document.getElementById('video');
+      let newTarget = Number(target) + 1 <= 3 ? Number(target) + 1 : 0;
+      let selected = document.getElementById(newTarget);
+      $('.selected').removeClass('selected');
+      selected.classList.add('selected');
+
+      // Fade Out Current Selection
+      $('#video').fadeOut();
+      $('#video').fadeOut('slow');
+      $('#video').fadeOut(2000);
       videoPlayer.pause();
-      // Key must be converted to Number here because of JavaScript string concatentation rules
-      currentVid.src = videoCache[Number(target) + 1] ? videoCache[Number(target) + 1] : videoCache[0];
-      videoPlayer.load();
-      videoPlayer.play();
+
+      // Set Time Out for Fade-In of Next Video
+      setTimeout(() => {
+        // Resolve New Source URL and Handle Possible Edge Case of Being at End of Map
+        currentVid.src = videoCache[Number(target) + 1] ? videoCache[Number(target) + 1] : videoCache[0];
+        videoPlayer.load();
+        videoPlayer.play();
+        $('#video').fadeIn();
+      }, 500);
     }
   });
 };
-
-// console.log(e);
-// if (e.keyCode == 37) {
-//   let videoPlayer = document.getElementById('video');
-//   videoPlayer.pause();
-//   let currentVid = document.getElementById('videoSource');
-//   currentVid.src = videoCache[i - 1] ? videoCache[i - 1] : videoCache[3];
-//   videoPlayer.load();
-//   videoPlayer.play();
-// } else if (e.keyCode == 39) {
-//   let videoPlayer = document.getElementById('video');
-//   videoPlayer.pause();
-//   let currentVid = document.getElementById('videoSource');
-//   currentVid.src = videoCache[i + 1] ? videoCache[i + 1] : videoCache[0];
-//   videoPlayer.load();
-//   videoPlayer.play();
-// }
